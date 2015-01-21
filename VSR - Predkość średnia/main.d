ccdc65a63e3bd09fe35b1955e737911a78ae30e7
KrzaQ <git@krzaq.cc>
2015-01-21 12:16:03 +0100

    VSR done.

---
+++ VSR - Predkość średnia/main.d
@@ -0,0 +1,24 @@
+import std.stdio;
+import std.regex;
+import std.string;
+import std.algorithm;
+import std.format;
+import std.conv;
+import std.typecons;
+import std.bigint;
+import std.functional;
+import std.math;
+
+void main()
+{
+	int num = stdin.byLine.front.to!int;
+
+	for(int i = 0; i < num; ++i){
+		auto split = stdin.byLine.front.split;
+		auto x = split[0].to!int;
+		auto y = split[1].to!int;
+
+		int ret = 2*x*y/(x+y);
+		ret.writeln;
+	}
+}
