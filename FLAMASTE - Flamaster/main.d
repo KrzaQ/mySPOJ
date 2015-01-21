d8c9a9f6397e15c030bc5a7598b0443b22e4ecb7
KrzaQ <git@krzaq.cc>
2015-01-21 11:51:00 +0100

    Removed dead code.

--- FLAMASTE - Flamaster/main.d
+++ FLAMASTE - Flamaster/main.d
@@ -9,39 +9,10 @@ import std.bigint;
 import std.functional;
 import std.math;
 
-struct Result
-{
-	int value;
-	int calculations;
-}
-
-bool isPalindrome(string s)
-{
-	int max = s.length/2;
-	for(int i = 0; i <= max; ++i){
-		if(s[i] != s[$ - 1 - i]) return false;
-	}
-	return true;
-}
-
-Result checkNumber(int val)
-{
-	string s = val.to!string;
-
-	if(s.isPalindrome){
-		return Result(val, 0);
-	}else{
-		auto r = memoize!checkNumber(s.to!(char[]).reverse.to!int + val);
-		r.calculations++;
-		return r;
-	}
-}
-
 void main()
 {
 	int num = stdin.byLine.front.to!int;
 
-	//auto r = ctRegex!r"(\w)(\$1+)";
 	auto r = ctRegex!r"(\w)\1(\1+)";
 
 	for(int i = 0; i < num; ++i){
