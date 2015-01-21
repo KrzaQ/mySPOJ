335d58f22dfa7bfe831e08115df6835eb4e1e698
KrzaQ <git@krzaq.cc>
2015-01-21 11:45:25 +0100

    FLAMASTE done.

---
+++ FLAMASTE - Flamaster/main.d
@@ -0,0 +1,54 @@
+import std.stdio;
+import std.regex;
+import std.string;
+import std.algorithm;
+import std.format;
+import std.conv;
+import std.typecons;
+import std.bigint;
+import std.functional;
+import std.math;
+
+struct Result
+{
+	int value;
+	int calculations;
+}
+
+bool isPalindrome(string s)
+{
+	int max = s.length/2;
+	for(int i = 0; i <= max; ++i){
+		if(s[i] != s[$ - 1 - i]) return false;
+	}
+	return true;
+}
+
+Result checkNumber(int val)
+{
+	string s = val.to!string;
+
+	if(s.isPalindrome){
+		return Result(val, 0);
+	}else{
+		auto r = memoize!checkNumber(s.to!(char[]).reverse.to!int + val);
+		r.calculations++;
+		return r;
+	}
+}
+
+void main()
+{
+	int num = stdin.byLine.front.to!int;
+
+	//auto r = ctRegex!r"(\w)(\$1+)";
+	auto r = ctRegex!r"(\w)\1(\1+)";
+
+	for(int i = 0; i < num; ++i){
+		//auto split = stdin.byLine.front.split;
+		//auto x = split[0].to!int;
+		//auto y = split[1].to!int;
+		auto l = stdin.byLine.front;
+		replaceAll!(v => "%s%d".format(v[1], v[2].length+2))(l, r).writeln;
+	}
+}
