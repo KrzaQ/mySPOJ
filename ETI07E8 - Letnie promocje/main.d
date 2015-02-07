b72e83e90bf8e4ba32b7be9771513743b04213f8
KrzaQ <github@krzaq.cc>
2015-02-07 23:54:29 +0100

    More tests.

--- ETI07E8 - Letnie promocje/main.d
+++ ETI07E8 - Letnie promocje/main.d
@@ -1,5 +1,6 @@
 import std.algorithm;
 import std.bigint;
+import std.container;
 import std.conv;
 import std.format;
 import std.functional;
@@ -57,13 +58,14 @@ void main()
 	//int num = stdin.byLine.front.to!int;
 
 	//alias StringList = Appender!(string[]);
-	alias StringList = string[];
-	alias Number = ulong;
+	//alias StringList = string[];
+	alias StringList = RedBlackTree!string;
+	alias Number = long;
 
 	StringList[Number] dict;
 
-	int words = stdin.extract!int;
-	int numbers = stdin.extract!int;
+	long words = stdin.extract!long;
+	long numbers = stdin.extract!long;
 
 	auto foo = function int(dchar c){
 		//c.writeln;
@@ -72,29 +74,31 @@ void main()
 		return r;
 	};
 
-	for(int i = 0; i < words; ++i){
-		auto str = stdin.extract!string;
+	for(long i = 0; i < words; ++i){
+		auto str = stdin.extract!string.filter!(isLower);
 
 		//"'%s'".format(str).writeln;
 
 		auto val = str.map!(foo).map!(to!string).join.to!Number;
 		//"%10s -> %-10s".format(str,val).writeln;
 		//dict[val].put(str);
-		dict[val] ~= str;
-		auto tmp = dict[val].sort.uniq;
-		tmp.copy(dict[val]);
+		auto it = val in dict;
+		if(!it) dict[val] = new StringList;
+		dict[val].stableInsert(str.to!string);
+		//auto tmp = dict[val].sort.uniq;
+		//tmp.copy(dict[val]);
 		//dict[val].sort.copy(dict[val]);
 	}
 
-	dict.writeln;
+	//dict.writeln;
 
-	for(int i = 0; i < numbers; ++i){
+	for(long i = 0; i < numbers; ++i){
 
 		auto val = stdin.extract!Number;
 
 		StringList* data = val in dict;
 
-		if(data) (*data).join(" ").writeln;
+		if(data) (*data)[].join(" ").writeln;
 		else "BRAK".writeln;
 	}
 }
